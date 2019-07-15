11bf77a09286d181a7201d01a9be42afa9897bf7
Amy Phung <phung.amy85@gmail.com>
2019-07-15 10:03:29 -0700

    initial commit

---
+++ app/src/main/java/com/ninou/dreamboat/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.ninou.dreamboat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button loginButton;
+    private Button createAcctButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.email_sign_in_button);
+        createAcctButton = findViewById(R.id.go_to_create_account_button);
+
+        createAcctButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createAcctButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent(LoginActivity.this, CreateAccountActivity.class));
+                    }
+                });
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/ninou/dreamboat/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ninou.dreamboat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button getStartedButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getStartedButton = findViewById(R.id.startButton);
+
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //go to LoginActivity
+                startActivity(new Intent(MainActivity.this,
+                        LoginActivity.class));
+            }
+        });
+    }
+}
