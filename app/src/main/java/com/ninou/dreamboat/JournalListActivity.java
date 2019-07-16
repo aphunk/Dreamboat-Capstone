52c0cda54cc25cb5b2e4628d4bccdfeb50bc1ca8
Amy Phung <phung.amy85@gmail.com>
2019-07-15 23:16:44 -0700

    displays list of journal entries

--- app/src/main/java/com/ninou/dreamboat/JournalListActivity.java
+++ app/src/main/java/com/ninou/dreamboat/JournalListActivity.java
@@ -4,14 +4,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import model.Journal;
+import ui.JournalRecyclerAdapter;
+import util.JournalApi;
 
 public class JournalListActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
@@ -19,14 +36,29 @@ public class JournalListActivity extends AppCompatActivity {
     private FirebaseUser user;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private StorageReference storageReference;
+    private List<Journal> journalList;
+    private RecyclerView recyclerView;
+    private JournalRecyclerAdapter journalRecyclerAdapter;
+
+    private CollectionReference collectionReference = db.collection("Journal");
+    private TextView noJournalEntry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_list);
 
+        Objects.requireNonNull(getSupportActionBar()).setElevation(0);
         firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
+
+        noJournalEntry = findViewById(R.id.list_no_thoughts);
+
+        journalList = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
@@ -64,5 +96,36 @@ public class JournalListActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart(); //get journals from firestore
+
+        collectionReference.whereEqualTo("userId", JournalApi.getInstance()
+                .getUserId())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if (!queryDocumentSnapshots.isEmpty()) {
+                            for (QueryDocumentSnapshot journals : queryDocumentSnapshots) {
+                                Journal journal = journals.toObject(Journal.class);
+                                journalList.add(journal);
+                            }
+
+                            //Invoke recyclerview
+                            journalRecyclerAdapter = new JournalRecyclerAdapter(JournalListActivity.this,
+                                    journalList);
+                            recyclerView.setAdapter(journalRecyclerAdapter);
+                            journalRecyclerAdapter.notifyDataSetChanged();
+
+                        }else {
+                            noJournalEntry.setVisibility(View.VISIBLE);
+
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
     }
 }
---
+++ app/src/main/java/ui/JournalRecyclerAdapter.java
@@ -0,0 +1,77 @@
+package ui;
+
+import android.content.Context;
+import android.text.format.DateUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.Timestamp;
+import com.ninou.dreamboat.R;
+
+import java.util.List;
+
+import model.Journal;
+
+public class JournalRecyclerAdapter extends RecyclerView.Adapter<JournalRecyclerAdapter.ViewHolder> {
+    private Context context;
+    private List<Journal> journalList;
+
+    public JournalRecyclerAdapter(Context context, List<Journal> journalList) {
+        this.context = context;
+        this.journalList = journalList;
+    }
+
+    @NonNull
+    @Override
+    public JournalRecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(context)
+                .inflate(R.layout.journal_row, viewGroup, false);
+
+        return new ViewHolder(view, context);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull JournalRecyclerAdapter.ViewHolder viewHolder, int position) {
+
+        Journal journal = journalList.get(position);
+
+        viewHolder.title.setText(journal.getTitle());
+        viewHolder.entries.setText(journal.getEntry());
+//        viewHolder.name.setText(journal.getUserName());
+        String timeAgo = (String) DateUtils.getRelativeTimeSpanString(journal
+                .getTimeAdded()
+                .getSeconds() * 1000);
+        viewHolder.dateAdded.setText(timeAgo);
+    }
+
+    @Override
+    public int getItemCount() {
+        return journalList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView
+                title,
+                entries,
+                dateAdded,
+                name;
+        String userId;
+        String username;
+
+
+        public ViewHolder(@NonNull View itemView, Context ctx) {
+            super(itemView);
+            context = ctx;
+
+            title = itemView.findViewById(R.id.journal_title_list);
+            entries = itemView.findViewById(R.id.journal_entry_list);
+            dateAdded = itemView.findViewById(R.id.journal_timestamp_list);
+//            name = itemView.findViewsWithText(R.id.username_acct);
+        }
+    }
+}
