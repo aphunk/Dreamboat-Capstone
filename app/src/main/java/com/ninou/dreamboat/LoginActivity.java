65f74b5d1bbf0c78d0dc9a7c7182523a54723ada
Amy Phung <phung.amy85@gmail.com>
2019-07-15 17:33:59 -0700

    add login functionality

--- app/src/main/java/com/ninou/dreamboat/LoginActivity.java
+++ app/src/main/java/com/ninou/dreamboat/LoginActivity.java
@@ -1,27 +1,60 @@
 package com.ninou.dreamboat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Text;
+
+import javax.annotation.Nullable;
+
+import util.JournalApi;
 
 public class LoginActivity extends AppCompatActivity {
     private Button loginButton;
     private Button createAcctButton;
 
+    private AutoCompleteTextView emailAddress;
+    private EditText password;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        emailAddress = findViewById(R.id.email);
+        password = findViewById(R.id.password);
 
         loginButton = findViewById(R.id.email_sign_in_button);
         createAcctButton = findViewById(R.id.go_to_create_account_button);
@@ -38,5 +71,64 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                loginEmailPasswordUser(emailAddress.getText().toString().trim(),
+                        password.getText().toString().trim());
+            }
+
+        });
+    }
+    private void loginEmailPasswordUser(String email, String password) {
+
+        if (!TextUtils.isEmpty(password)
+        && !TextUtils.isEmpty(email)) {
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            assert user != null;
+                            final String currentUserId = user.getUid();
+
+                            collectionReference
+                                    .whereEqualTo("userId", currentUserId)
+                                    .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots,
+                                                            @Nullable FirebaseFirestoreException e) {
+                                            if (e != null) {
+                                            }
+                                            assert queryDocumentSnapshots != null;
+                                            if (!queryDocumentSnapshots.isEmpty()) {
+
+                                                for (QueryDocumentSnapshot snapshot : queryDocumentSnapshots) {
+                                                    JournalApi journalApi = JournalApi.getInstance();
+                                                    journalApi.setUsername(snapshot.getString("username"));
+                                                    journalApi.setUserId(currentUserId);
+
+                                                    //Go to ListActivity
+                                                    startActivity(new Intent(LoginActivity.this,
+                                                            PostJournalActivity.class));
+                                                }
+                                                }
+
+                                        }
+                                    });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+        }else {
+            Toast.makeText(LoginActivity.this, "Enter an email and password",
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 }
