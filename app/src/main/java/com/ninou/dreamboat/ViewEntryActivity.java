cf1568f2d0145ffde17d6a58620284b8bc8dc3c2
Amy Phung <phung.amy85@gmail.com>
2019-07-19 11:20:43 -0700

    create journal entry view

---
+++ app/src/main/java/com/ninou/dreamboat/ViewEntryActivity.java
@@ -0,0 +1,14 @@
+package com.ninou.dreamboat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_entry);
+    }
+}
